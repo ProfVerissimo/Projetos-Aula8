5c0fecf986246e47e051ef35b9750d3b8547fa99
Veríssimo <98069941+ProfVerissismo@users.noreply.github.com>
2022-01-23 18:14:59 -0300

    alteração feita - Remotamente no GitHub

--- appFinanceiro.py
+++ appFinanceiro.py
@@ -1,4 +1,5 @@
 print ("*------------------------------------------*")
 print ("*  Bem Vindo Ao noss App Gestão Finacneira *")
+print("*Linha Incluída no GitHub - Remotamente     *")
 print ("*------------------------------------------*")
 #
